ce3e27874d447ec0253818a18d54e2c13477cd88
Mary_Healy <mary.healy@expathousingnetwork.nl>
2020-07-01 14:27:11 +0200

    Base appareal changes

--- resources/views/base-apparel/index.blade.php
+++ resources/views/base-apparel/index.blade.php
@@ -17,12 +17,12 @@
           </div>
           <div class="base-form">
             <form id="email-form" action="">
-              <input id="email-input" type="email" name="email" placeholder="Email Address" aria-labelledby>
-              <span class="error-icon"><img src="{{ asset('img/base-apparel/icon-error.svg')}}" alt=""></span>
-              <button id="email-submit" type="submit"><img src="{{ asset('img/base-apparel/icon-arrow.svg') }}" alt="arrow"></button>
-              <div class="error-msg">
-                Please provide a valid email
+              <div class="form-input">
+                <input id="email-input" type="text" name="email" placeholder="Email Address" aria-labelledby>
+                <span class="error-icon"><img src="{{ asset('img/base-apparel/icon-error.svg')}}" alt=""></span>
+                <small class="error-msg">Please provide a valid email</small>
               </div>
+              <button id="email-submit" type="submit"><img src="{{ asset('img/base-apparel/icon-arrow.svg') }}" alt="arrow"></button>
             </form>
           </div>
         </div>
@@ -37,26 +37,59 @@
 
 @section('custom-scripts')
   <script>
-    const submitBtn = document.querySelector('#email-submit');
-    const email = document.querySelector('#email-input');
-    const errorIcon = document.querySelector('.error-icon');
-    const errorMsg = document.querySelector('.error-msg');
-    const emailValid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    function validate(){
-      let emailValue = email.value;
+    const form = document.getElementById('email-form')
+    const email = document.getElementById('email-input');
 
-      if(!emailValid.test(emailValue)){
-        errorIcon.style.display = 'block';
-        errorMsg.style.display = 'block';
-        email.style.border = '1px solid #f96262';
-      } else {
-        console.log('yeah')
-      }
-      return false
+    function showError(input, msg){
+      const formInput = input.parentElement;
+      formInput.classList.add('error');
+      const small = formInput.querySelector('small');
+      small.innerText = msg;
     }
 
-    submitBtn.addEventListener('click', validate);
+    function checkEmail(input){
+      const regexEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+      if (!regexEmail.test(input.value.trim())) {
+        showError(input, 'Please enter a valid email');
+      } else {
+        showSuccess(input);
+      }
+    }
+
+    function showSuccess(input){
+      const formInput = input.parentElement;
+      formInput.classList.add('success');
+      formInput.classList.remove('error');
+    }
+
+    form.addEventListener('submit', function(event){
+      event.preventDefault();
+
+      checkEmail(email);
+    });
+
+    // const submitBtn = document.querySelector('#email-submit');
+    // const email = document.querySelector('#email-input');
+    // const errorIcon = document.querySelector('.error-icon');
+    // const errorMsg = document.querySelector('.error-msg');
+    // const emailValid = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    // function validate(){
+    //   let emailValue = email.value;
+
+    //   if(!emailValid.test(emailValue)){
+    //     errorIcon.style.display = 'block';
+    //     errorMsg.style.display = 'block';
+    //     email.style.border = '1px solid #f96262';
+    //   } else {
+    //     console.log('yeah')
+    //   }
+    //   return false
+    // }
+
+    // submitBtn.addEventListener('click', validate);
 
 
   </script>
